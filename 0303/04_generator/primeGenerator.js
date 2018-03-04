e4734e149e3abb5e933c5ce10c5bc98fdaed4ad4
haeguri <haegyun821@gmail.com>
2018-03-04 19:00:59 +0900

    iterator, generator init

---
+++ 0303/04_generator/primeGenerator.js
@@ -0,0 +1,5 @@
+function primeGenerator(){
+
+}
+
+export default primeGenerator;
---
+++ 0303/app.js
@@ -0,0 +1,4 @@
+import primeIterator from './03_iterator/primeIterator.js';
+import primeGenerator from './04_generator/primeGenerator.js';
+
+// primeIterator test.
